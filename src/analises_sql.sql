70cce07d3bae5e262c0b2631ae4cf2d355cc5917
VALTER CARLOS AGUIAR <77332024+valteraguiar@users.noreply.github.com>
2026-07-29 22:59:30 -0300

    Update analises_sql.sql

--- src/analises_sql.sql
+++ src/analises_sql.sql
@@ -1,1 +1,51 @@
 
+-- =========================================
+-- TOP 10 MARCAS POR PREÇO MÉDIO
+-- =========================================
+
+SELECT
+    brand,
+    ROUND(AVG(avg_price_brl), 2) AS preco_medio
+FROM carros
+GROUP BY brand
+ORDER BY preco_medio DESC
+LIMIT 10;
+
+
+-- =========================================
+-- TOP 10 MARCAS MAIS PRESENTES
+-- =========================================
+
+SELECT
+    brand,
+    COUNT(*) AS quantidade
+FROM carros
+GROUP BY brand
+ORDER BY quantidade DESC
+LIMIT 10;
+
+
+-- =========================================
+-- PREÇO MÉDIO POR COMBUSTÍVEL
+-- =========================================
+
+SELECT
+    fuel,
+    COUNT(*) AS quantidade,
+    ROUND(AVG(avg_price_brl), 2) AS preco_medio
+FROM carros
+GROUP BY fuel
+ORDER BY preco_medio DESC;
+
+
+-- =========================================
+-- PREÇO MÉDIO POR CÂMBIO
+-- =========================================
+
+SELECT
+    gear,
+    COUNT(*) AS quantidade,
+    ROUND(AVG(avg_price_brl), 2) AS preco_medio
+FROM carros
+GROUP BY gear
+ORDER BY preco_medio DESC;
